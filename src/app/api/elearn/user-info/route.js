8998bd837344cca94778aa1786eee3806f17346d
falachabt <bennytenezeu@gmail.com>
2025-07-18 08:13:27 +0200

    set runtime environment to Node.js in user-info API endpoint

--- src/app/api/elearn/user-info/route.js
+++ src/app/api/elearn/user-info/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const runtime = 'nodejs';
+
 export async function POST(request) {
     try {
         const authHeader = request.headers.get('authorization');
